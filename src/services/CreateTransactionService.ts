8b29db84e0663898fccc47bae56da1d24d018e42
Rodrigo A. Costa <razevedocosta@gmail.com>
2020-11-17 13:05:07 -0400

    funcionalidades implementadas e corrigidas

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,13 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+// interface para determinar a tipagem dos atributos
+interface Request{
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +15,27 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({title, value, type}: Request): Transaction {
+    
+  if (!["income", "outcome"].includes(type)) {
+    throw new Error('Transaction type is invalid');
+  }
+
+    const { total } = this.transactionsRepository.getBalance();
+
+    if ( type == 'outcome' && total < value) {
+      
+      throw new Error('You do not have enough balance');
+    }
+
+
+    const transaction = this.transactionsRepository.create({
+      title, 
+      value, 
+      type,
+    });
+
+    return transaction;
   }
 }
 
